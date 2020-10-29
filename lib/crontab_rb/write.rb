e2d8b26694b0f416344c9ad31680462329a9c19b
Doan Trung Quan <quan.doan@tomosia.com>
2020-10-29 15:39:47 +0700

    add stage

--- lib/crontab_rb/write.rb
+++ lib/crontab_rb/write.rb
@@ -12,7 +12,7 @@ module CrontabRb
           job_template: "/bin/bash -l -c ':job'",
           template: "cd :path && :bundle_command :runner_command -e :environment ':task' :output",
           environment_variable: 'RAILS_ENV',
-          environment: 'production',
+          environment: :stage,
           path: Dir.pwd,
           chronic_options: {},
           runner_command: "bin/rails runner",
